5ea6e00dc61c12366b85dd917011a3d084290806
gengyaqi <1329379869@qq.com>
2019-11-06 22:47:35 +0800

    add github authorize

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 ## 码匠社区
 
 ## 资料
-https://spring.io/guides/gs/serving-web-content/#use-maven
-
-https://spring.io/guides
-
-https://elasticsearch.cn/
+[Spring Web 文档](https://spring.io/guides/gs/serving-web-content/#use-maven)  
+[Spring 文档](https://spring.io/guides)  
+[ES中文](https://elasticsearch.cn/)  
+[Github OAuth 文档](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 
 ## 工具
 
-https://git-scm.com/download
-
-https://v3.bootcss.com/
-
-https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
+[Git 下载](https://git-scm.com/download)  
+[Bootstrap文档](https://v3.bootcss.com/)
---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,35 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @program: community
+ * @description:
+ **/
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("a2627224512e07ec0611");
+        accessTokenDTO.setClient_secret("7f905a8c6c948754ef16df6c400b2da5ce47b281");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
